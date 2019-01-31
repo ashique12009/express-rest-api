99a964bcd4fd8d475d6c23ff70b2c033f5cb9bf6
ashique <ashique12009@gmail.com>
2019-01-31 14:57:32 +0600

    Server code update with new package added

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const contactRoute = require('./api/routes/contact');
 
-const PORT = process.env.PORT || 3000;
+// App/package using initialising related code 
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Route related code
+app.use('/api/contact', contactRoute);
 
 app.get('/', (req, res) => {
     res.send('<div><h1>Welcome to home page ok</h1></div>');
 });
+
+// Server related code
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port : ${PORT}`);
