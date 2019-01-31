035756efbb774afc18ec825a20f481f52dd047f6
ashique <ashique12009@gmail.com>
2019-01-31 15:27:34 +0600

    CORS pacakge added

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const contactRoute = require('./api/routes/contact');
+const cors = require('cors');
 
 // App/package using initialising related code 
 const app = express();
 app.use(morgan('dev'));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
