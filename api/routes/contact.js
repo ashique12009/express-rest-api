003c62cce0e206eff745f1c13b033df1181ffb6c
ashique <ashique12009@gmail.com>
2019-02-01 15:08:39 +0600

    Update contact route with authenticate

--- api/routes/contact.js
+++ api/routes/contact.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const contactController = require('../controllers/contact');
+const authenticate = require('../middleware/authenticate');
 
 router.get('/', contactController.getAllContactController);
 
-router.post('/', contactController.postNewContactController);
+router.post('/', authenticate, contactController.postNewContactController);
 
 router.get('/:id', contactController.getSingleContact);
 
-router.put('/:id', contactController.editContact);
+router.put('/:id', authenticate, contactController.editContact);
 
-router.delete('/:id', contactController.deleteContact);
+router.delete('/:id', authenticate, contactController.deleteContact);
 
 module.exports = router;
