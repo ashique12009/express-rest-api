80108c06abf6c6b9cd49f1da5dc2248989a259eb
ashique <ashique12009@gmail.com>
2019-02-01 15:06:28 +0600

    Update Authentication applied to users GET route

--- api/middleware/authenticate.js
+++ api/middleware/authenticate.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const authenticate = (req, res, next) => {
     try {
-        const token = req.header.authorization.split(' ');
+        const token = req.headers.authorization.split(' ')[1];
         const decode = jwt.verify(token, 'SECRET');
 
         req.user = decode;
